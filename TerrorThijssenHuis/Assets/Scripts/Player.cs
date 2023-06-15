d9fde529b4c2684ed55887524f20891596da8a88
Luuk <luuknoverraz@gmail.com>
2023-06-15 16:07:51 +0200

    Added text when close to door

--- TerrorThijssenHuis/Assets/Scripts/Player.cs
+++ TerrorThijssenHuis/Assets/Scripts/Player.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float movementSpeed = 2;
 
-    List<string> inventory = new List<string>();
+    private List<string> inventory = new List<string>();
+    private bool nearDoor = false;
+
+    public Text interactionText;
+    private int interactionTextOffsetX = 125;
+    private int interactionTextOffsetY = 50;
 
     // Start is called before the first frame update
     private void Start()
@@ -19,6 +25,32 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Move();
+
+        if (nearDoor)
+        {
+            interactionText.text = "Press 'E' to open door";
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                for (int i = 0; i < inventory.Count; i++)
+                {
+                    if (inventory[i] == "Key")
+                    {
+                        // Open door (replace later with more suitable behavior)
+                        Destroy(GameObject.FindWithTag("Door"));
+                    }
+                }
+            }
+        }
+        else 
+        {
+            interactionText.text = "";
+        }
+    }
+     
+    public void LateUpdate()
+    {
+        interactionText.transform.position = new Vector2(Camera.main.WorldToScreenPoint(transform.position).x + interactionTextOffsetX, 
+                                                        Camera.main.WorldToScreenPoint(transform.position).y + interactionTextOffsetY);
     }
 
     private void Move()
@@ -36,6 +68,19 @@ public class Player : MonoBehaviour
             Destroy(other.gameObject);
             inventory.Add("Key");
             // Add string Key to inventory list
+        }
+
+        if (other.tag == "Door")
+        {
+            nearDoor = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Door")
+        {
+            nearDoor = false;
         }
     }
 }
